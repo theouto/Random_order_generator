a74a7855f305738d1edf2b6feec7400044d0e1e3
theouto <tomas@otero.pro>
2026-03-10 11:40:10 +0100

    true random via rdrand, or as close to random as a computer can get

--- random.cpp
+++ random.cpp
@@ -1,6 +1,34 @@
+#include <cstddef>
 #include <iostream>
 #include <vector>
+#include <cstdint>
+#include <math.h>
+
 using namespace std;
+
+//https://github.com/jtulak/RdRand/
+int RDRAND16_STEP(uint16_t *x)
+{
+  unsigned char err = 1;
+  asm volatile (".byte 0x66; .byte 0x0f; .byte 0xc7; .byte 0xf0; setc %1"
+                      : "=a" (*x), "=qm" (err));
+  if(err == 1)
+  {
+    return 1;
+  }
+  return 0;
+}
+
+double trurand()
+{
+  uint16_t seed;
+  if (RDRAND16_STEP(&seed))
+  {
+    return abs(sin(seed));
+  }
+
+  return 0;
+}
 
 bool yesorno = true;
 int bottom, ceiling;
@@ -15,16 +43,19 @@ int main()
     vector<int> blacklist;
     for (int j = 0; j < ceiling-bottom; j)
     {
-        int rando = rand() % (ceiling-bottom) + bottom;
+        int rando = (int)(trurand() * (double)(ceiling-bottom) + bottom);
         for (auto& c : blacklist)
         {
             if (rando == c)
             {
                 yesorno = false;
+                break;
             }
         }
+
         if (yesorno)
         {
+            if (rando < bottom && rando > ceiling) continue;
             cout << rando << endl;
             blacklist.push_back(rando);
             j++;
